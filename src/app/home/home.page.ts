1147c8bc47c4cf4ce2b0b044fd1221846fad2a8f
agus <reza.agustiana@gmail.com>
2020-11-10 20:21:23 +0700

    Implementing Firebase Analytics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
+import { AnalyticsService } from '../services/analytics.service';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  enabled = this.analyticsService.analyticsEnabled;
+ 
+  constructor(private analyticsService: AnalyticsService) { }
+ 
+  setUser() {
+   this.analyticsService.setUser();
+  }
+ 
+  setProperty() {
+    this.analyticsService.setProperty();
+  }
+ 
+  logEvent() {
+    this.analyticsService.logEvent();
+  }
+ 
+ 
+  toggleDataCollection() {
+    this.analyticsService.toggleAnalytics();
+    this.enabled = this.analyticsService.analyticsEnabled;
+  }
+ 
 }
--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from './../../environments/environment';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+ 
+// Init for the web
+import "@capacitor-community/firebase-analytics";
+ 
+import { Plugins } from "@capacitor/core";
+const { FirebaseAnalytics, Device } = Plugins;
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsService {
-
-  constructor() { }
+  analyticsEnabled = true;
+ 
+  constructor( private router: Router) {
+    this.initFb();
+    this.router.events.pipe(
+      filter((e: RouterEvent) => e instanceof NavigationEnd),
+    ).subscribe((e: RouterEvent) => {
+      console.log('route changed: ', e.url);
+      this.setScreenName(e.url)
+    });
+  }
+ 
+  async initFb() {
+    if ((await Device.getInfo()).platform == 'web') {
+      FirebaseAnalytics.initializeFirebase(environment.firebaseConfig);
+    }
+  }
+ 
+  setUser() {
+    // Use Firebase Auth uid
+    FirebaseAnalytics.setUserId({
+      userId: "test_123",
+    });
+  }
+ 
+  setProperty() {
+    FirebaseAnalytics.setUserProperty({
+      name: "framework",
+      value: "angular",
+    });
+  }
+ 
+  logEvent() {
+    FirebaseAnalytics.logEvent({
+      name: "indihome_event",
+      params: {
+        email: "reza.agustiana@telkom.co.id",
+        phone: "085156956147"
+      }
+    });
+  }
+ 
+  setScreenName(screenName) {
+    FirebaseAnalytics.setScreenName({
+      screenName
+    });
+  }
+ 
+  toggleAnalytics() {
+    this.analyticsEnabled = !this.analyticsEnabled;
+    FirebaseAnalytics.setCollectionEnabled({
+      enabled: this.analyticsEnabled,
+    });    
+  }
+  
 }
